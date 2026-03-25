3e603227bbc620d2e10e026175dd5ca2181fecd5
abid3633ali-sys <abid3633ali@gmail.com>
2026-03-25 02:00:31 -0100

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 import requests
 
-# بياناتك الصحيحة التي نجحت في VS Code
 TOKEN = "8343143091:AAHua7xDAYqwIExQYn3p1130W6rrbaRed7E"
 CHAT_ID = "7665460040"
 
 def send_test():
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    text = "مرحبا يا علي، أنا أعمل الآن من داخل GitHub بكفاءة! 🚀"
-    
+    text = "مرحبا يا علي، أنا أعمل الآن من داخل GitHub بكفاءة قوية جداً! 🚀"
     payload = {"chat_id": CHAT_ID, "text": text}
     
-    print("⏳ جاري محاولة الإرسال من خوادم GitHub...")
+    # محاولة الإرسال
     response = requests.post(url, data=payload)
-    
     if response.status_code == 200:
-        print("✅ نجحت العملية! تحقق من هاتفك.")
+        print("✅ نجحت العملية! الرسالة في طريقها لهاتفك.")
     else:
-        print(f"❌ فشل الإرسال. كود الخطأ: {response.status_code}")
-        print(f"السبب: {response.text}")
+        print(f"❌ فشل GitHub في الإرسال. الخطأ: {response.text}")
 
 if __name__ == "__main__":
     send_test()
